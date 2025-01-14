80a3e0a6b9e6e9a1740116b44d2667c6fd8acce1
Andras Bacsai <andras.bacsai@gmail.com>
2025-01-14 14:37:16 +0100

    fix: add x-cloak attribute to tooltip component for improved visibility handling

--- resources/views/components/tooltip/content.blade.php
+++ resources/views/components/tooltip/content.blade.php
@@ -11,7 +11,7 @@
     $alignment = $side . ['center' => '', 'end' => '-end', 'start' => '-start'][$align];
 @endphp
 
-<div x-tooltip:content x-transition:enter.delay.{{ $delayDuration }}
+<div x-tooltip:content x-transition:enter.delay.{{ $delayDuration }} x-cloak
     x-anchor.{{ $alignment }}.offset.{{ $sideOffset }}="document.getElementById($id('alpine-tooltip'))"
     {{ $attributes->twMerge('z-50 overflow-hidden rounded-md px-3 py-1.5 text-xs bg-background text-primary-foreground') }}>
     {{ $slot }}
