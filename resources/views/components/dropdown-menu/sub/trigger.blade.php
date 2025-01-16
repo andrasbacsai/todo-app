63ea74843be2e547712b43583e4a5961739824dc
Andras Bacsai <andras.bacsai@gmail.com>
2025-01-16 10:09:34 +0100

    refactor: streamline dropdown menu trigger markup for improved readability and maintainability

--- resources/views/components/dropdown-menu/sub/trigger.blade.php
+++ resources/views/components/dropdown-menu/sub/trigger.blade.php
@@ -2,20 +2,15 @@
     'disabled' => false,
     'inset' => false,
 ])
-<div
-    role="menuitem"
-    aria-haspopup="true"
-    aria-disabled="{{ $disabled ? 'true' : 'false' }}"
-    x-dropdown-menu:subbutton
-    tabindex="-1"
+<div role="menuitem" aria-haspopup="true" aria-disabled="{{ $disabled ? 'true' : 'false' }}" x-dropdown-menu:subbutton
+    tabindex="-1" @click.stop
     {{ $attributes->except(['x-on:click', '@click', 'wire:click'])->twMerge([
             'hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             'relative flex w-full cursor-default select-none items-center',
             'rounded-sm px-2 py-1.5 text-sm outline-none transition-colors',
             'opacity-50 cursor-not-allowed' => $disabled,
             'pl-8' => $inset,
-        ]) }}
->
+        ]) }}>
     <span>
         {{ $slot }}
     </span>
