1494a4e261a68d0d488cf1799236c344f5bed3a9
Andras Bacsai <andras.bacsai@gmail.com>
2025-01-27 13:44:40 +0100

    feat: Enhance Vite configuration with dynamic HMR host support

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    server: process.env.NODE_ENV === 'development' ? {
-        host: '0.0.0.0',
-        hmr: {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        server: process.env.NODE_ENV === 'development' ? {
             host: '0.0.0.0',
-        },
-    } : {},
+            hmr: {
+                host: env.VITE_HMR_HOST || '0.0.0.0',
+            },
+        } : {},
 
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+    };
 });
