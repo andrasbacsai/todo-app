bba12e2ddd25b42da93c36b154fe9280b1e771db
Andras Bacsai <andras.bacsai@gmail.com>
2025-01-28 08:46:18 +0100

    test pwa

---
+++ config/laravelpwa.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'name' => 'Jata',
+    'manifest' => [
+        'name' => 'Jata',
+        'short_name' => 'Jata',
+        'start_url' => '/',
+        'background_color' => '#ffffff',
+        'theme_color' => '#000000',
+        'display' => 'standalone',
+        'orientation' => 'any',
+        'status_bar' => 'black',
+        'icons' => [
+            '72x72' => [
+                'path' => '/images/icons/android-launchericon-72-72.png',
+                'purpose' => 'any',
+            ],
+            '96x96' => [
+                'path' => '/images/icons/android-launchericon-96-96.png',
+                'purpose' => 'any',
+            ],
+            '128x128' => [
+                'path' => '/images/icons/android-launchericon-128-128.png',
+                'purpose' => 'any',
+            ],
+            '144x144' => [
+                'path' => '/images/icons/android-launchericon-144-144.png',
+                'purpose' => 'any',
+            ],
+            '512x512' => [
+                'path' => '/images/icons/android-launchericon-512-512.png',
+                'purpose' => 'any',
+            ],
+
+        ],
+        'custom' => [],
+    ],
+];
